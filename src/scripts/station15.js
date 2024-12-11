e2cfda0251de392776ec1d8c7535a407ab129750
handakun <oci_code_git@copirobo.com>
2024-12-11 14:09:43 +0000

    終了

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -1,3 +1,5 @@
+const { bimap } = require("fp-ts/lib/pipeable");
+
 async function getData() {
   // Promiseを使った実装をやってみましょう！APIとの通信でよく使う表現になります！
   // ３秒後にフルネームのリストを表示されるようにしましょう。
@@ -6,11 +8,24 @@ async function getData() {
     { id: 1, first_name: '優', family_name: '大木', affiliation: 'TechTrain', is_student: false },
     { id: 2, first_name: '太郎', family_name: '山田', affiliation: 'HogeHoge大学', is_student: true }
   ];
-  const result = await test()
+  const result = await test(userList)
   return await result
 }
 
-function test() {
-  return
+function test(userList) {
+  return new Promise(b => {
+    setTimeout(() => {
+      let aa = document.getElementById(`result`);
+      for (let b of userList) {
+        let aaa = document.createElement(`li`);
+        // console.log(`「${b.family_name} + ' ' + ${b.first_name}」`);
+        //「」いらんやん...
+        aaa.innerText = `${b.family_name} ${b.first_name}`;
+        aa.appendChild(aaa);
+      }
+      b();
+    }, 3000);
+  });
 }
 
+getData();
